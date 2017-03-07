fb8bf9ffe1c26d961a06fbdff30b42f8038c6667
Ingo Bürk <ingo.buerk@tngtech.com>
2017-03-07 15:43:42 +0000

    cleanup

--- .i3/rofi
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-rofi -show run -lines 4 -eh 2 -width 1000 -padding 25 -hide-scrollbar -fuzzy -opacity "100" -bw 1 -font "Ubuntu Mono derivative Powerline Regular 16"
--- .i3/wallpaper_changer.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-WALLPAPERS="/home/josh/Pictures/Backgrounds"
-ALIST=( `ls -w1 $WALLPAPERS` )
-RANGE=${#ALIST[@]}
-let "number = $RANDOM"
-let LASTNUM="`cat $WALLPAPERS/.last` + $number"
-let "number = $LASTNUM % $RANGE"
-echo $number > $WALLPAPERS/.last
-feh --bg-fill $WALLPAPERS/${ALIST[$number]}
